81f2abc15776a9407f386338404df1072a0258b3
Sota Funabashi <sfunabashi276@gmail.com>
2021-05-15 00:06:53 +0900

    minor fix

--- Assets/Freyja/Demo/Demo1/Scripts/Editor/DemoSceneManagerEditor.cs
+++ Assets/Freyja/Demo/Demo1/Scripts/Editor/DemoSceneManagerEditor.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace Freyja.Demo.Demo1
+namespace Freyja.Demo.Demo1.Editor
 {
     [CustomEditor(typeof(DemoSceneManager))]
-    public class ExampleScriptEditor : Editor {
+    public class ExampleScriptEditor : UnityEditor.Editor {
         public override void OnInspectorGUI(){
             base.OnInspectorGUI ();
 
