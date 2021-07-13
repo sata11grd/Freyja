3f73a896e01986cac1fd7acdfaa052325f39d552
Sota Funabashi <sfunabashi276@gmail.com>
2021-07-13 22:18:46 +0900

    rename from App and Enclave into FreyApp and FreyEnclave

--- Assets/Freyja/Scripts/Core/Dll.cs
+++ Assets/Freyja/Scripts/Core/Dll.cs
@@ -7,20 +7,12 @@ namespace Freyja.Core
 {
     public static class Dll
     {
-        /*
-        [DllImport("App", CallingConvention = CallingConvention.StdCall)]
-        public static extern int add(int a, int b);
-
-        [DllImport("App", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int test();
-        */
-        
-        [DllImport("App", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-        [return: MarshalAs(UnmanagedType.LPStr)]
-        public static extern string frey_read_call(string frd_fpath, string decryption_key, string log_fpath);
-        
-        [DllImport("App", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [DllImport("FreyApp", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         [return: MarshalAs(UnmanagedType.LPStr)]
         public static extern void frey_write_call(string data, string frd_fpath, string encryption_key, string log_fpath);
+        
+        [DllImport("FreyApp", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern string frey_read_call(string frd_fpath, string decryption_key, string log_fpath);
     }
 }
--- Assets/Freyja/Tools/transporter.bat
+++ Assets/Freyja/Tools/transporter.bat
@@ -1,10 +1,10 @@
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\App.dll
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\App.dll.meta
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\Enclave.dll
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\Enclave.dll.meta
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\Enclave.signed.dll
-del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\Enclave.signed.dll.meta
 copy C:\Users\sfuna\Documents\repos\Freyja\Frey\x64\Debug C:\Users\sfuna\Documents\repos\Freyja
-copy C:\Users\sfuna\Documents\repos\Freyja\App.dll C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64
-copy C:\Users\sfuna\Documents\repos\Freyja\Enclave.dll C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyApp.dll
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyApp.dll.meta
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyEnclave.dll
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyEnclave.dll.meta
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyEnclave.signed.dll
+del C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64\FreyEnclave.signed.dll.meta
+copy C:\Users\sfuna\Documents\repos\Freyja\FreyApp.dll C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64
+copy C:\Users\sfuna\Documents\repos\Freyja\FreyEnclave.dll C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64
 copy C:\Users\sfuna\Documents\repos\Freyja\Enclave.signed.dll C:\Users\sfuna\Documents\repos\Freyja\Assets\Plugins\x84_64
